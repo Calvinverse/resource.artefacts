58e26b16bb8e34f8dc621fd5f875088f7af1b1bc
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-03-15 08:25:50 +1300

    Updating to the latest version of the firewall cookbook
    references #10

--- src/cookbooks/resource_artefacts/metadata.rb
+++ src/cookbooks/resource_artefacts/metadata.rb
@@ -13,7 +13,7 @@ version '${VersionSemantic}'
 
 supports 'ubuntu', '>= 16.04'
 
-depends 'firewall', '= 2.6.2'
+depends 'firewall', '= 2.6.3'
 depends 'java_se', '= 8.162.0'
 depends 'nexus3', '= 2.1.1'
 depends 'poise-service', '= 1.5.2'
