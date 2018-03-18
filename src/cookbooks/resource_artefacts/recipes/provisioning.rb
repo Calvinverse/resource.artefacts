a2a7ce09d5d0edae14884ff301d18112ad8fc0ba
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-03-18 23:04:40 +1300

    Enable the service during provisioning
    references #10

--- src/cookbooks/resource_artefacts/recipes/provisioning.rb
+++ src/cookbooks/resource_artefacts/recipes/provisioning.rb
@@ -7,6 +7,18 @@
 # Copyright 2017, P. van der Velde
 #
 
+file '/etc/init.d/provision_image.sh' do
+  action :create
+  content <<~BASH
+    #!/bin/bash
+
+    function f_provisionImage {
+      sudo systemctl enable nexus.service
+    }
+  BASH
+  mode '755'
+end
+
 service 'provision.service' do
   action [:enable]
 end
--- src/cookbooks/resource_artefacts/spec/provisioning_spec.rb
+++ src/cookbooks/resource_artefacts/spec/provisioning_spec.rb
@@ -6,6 +6,10 @@ describe 'resource_artefacts::provisioning' do
   context 'configures provisioning' do
     let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
+    it 'creates provision_image.sh in the /etc/init.d directory' do
+      expect(chef_run).to create_file('/etc/init.d/provision_image.sh')
+    end
+
     it 'enables the provisioning service' do
       expect(chef_run).to enable_service('provision.service')
     end
