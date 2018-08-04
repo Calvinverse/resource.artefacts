8a4f0fd50870e675c4fa7d67481f314c58f6e0ce
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-08-04 23:32:09 +1200

    Reload is not a keyword for systemctl, restart is

--- src/cookbooks/resource_artefacts/spec/nexus_metrics_spec.rb
+++ src/cookbooks/resource_artefacts/spec/nexus_metrics_spec.rb
@@ -123,7 +123,7 @@ describe 'resource_artefacts::nexus_metrics' do
         # command will only run if the resulting template changes. The command must
         # return within 30s (configurable), and it must have a successful exit code.
         # Consul Template is not a replacement for a process monitor or init system.
-        command = "chown telegraf:telegraf /etc/telegraf/telegraf.d/inputs_jolokia.conf && systemctl reload telegraf"
+        command = "chown telegraf:telegraf /etc/telegraf/telegraf.d/inputs_jolokia.conf && systemctl restart telegraf"
 
         # This is the maximum amount of time to wait for the optional command to
         # return. Default is 30s.
