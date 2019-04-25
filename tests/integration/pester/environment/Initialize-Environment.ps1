d063a474045833f2aa1ae0c05c31cd132939a0e4
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-04-26 10:24:03 +1200

    Updating test files

--- tests/integration/pester/environment/Initialize-Environment.ps1
+++ tests/integration/pester/environment/Initialize-Environment.ps1
@@ -1,5 +1,7 @@
 function Get-IpAddress
 {
+    $ErrorActionPreference = 'Stop'
+
     $output = & /sbin/ifconfig eth0
     $line = $output |
         Where-Object { $_.Contains('inet addr:') } |
@@ -12,21 +14,45 @@ function Get-IpAddress
 
 function Initialize-Environment
 {
-    Start-TestConsul
+    $ErrorActionPreference = 'Stop'
 
-    Install-Vault -vaultVersion '0.9.1'
-    Start-TestVault
+    try
+    {
+        Start-TestConsul
 
-    Write-Output "Waiting for 10 seconds for consul and vault to start ..."
-    Start-Sleep -Seconds 10
+        Install-Vault -vaultVersion '0.9.1'
+        Start-TestVault
 
-    Join-Cluster
+        Write-Output "Waiting for 10 seconds for consul and vault to start ..."
+        Start-Sleep -Seconds 10
 
-    Set-VaultSecrets
-    Set-ConsulKV
+        Join-Cluster
 
-    Write-Output "Giving consul-template 30 seconds to process the data ..."
-    Start-Sleep -Seconds 30
+        Set-VaultSecrets
+        Set-ConsulKV
+
+        Write-Output "Giving consul-template 30 seconds to process the data ..."
+        Start-Sleep -Seconds 30
+    }
+    catch
+    {
+        $currentErrorActionPreference = $ErrorActionPreference
+        $ErrorActionPreference = 'Continue'
+
+        try
+        {
+            Write-Error $errorRecord.Exception
+            Write-Error $errorRecord.ScriptStackTrace
+            Write-Error $errorRecord.InvocationInfo.PositionMessage
+        }
+        finally
+        {
+            $ErrorActionPreference = $currentErrorActionPreference
+        }
+
+        # rethrow the error
+        throw $_.Exception
+    }
 }
 
 function Install-Vault
@@ -36,12 +62,20 @@ function Install-Vault
         [string] $vaultVersion
     )
 
+    $ErrorActionPreference = 'Stop'
+
     & wget "https://releases.hashicorp.com/vault/$($vaultVersion)/vault_$($vaultVersion)_linux_amd64.zip" --output-document /test/vault.zip
     & unzip /test/vault.zip -d /test/vault
 }
 
 function Join-Cluster
 {
+    [CmdletBinding()]
+    param(
+    )
+
+    $ErrorActionPreference = 'Stop'
+
     Write-Output "Joining the local consul ..."
 
     # connect to the actual local consul instance
@@ -59,18 +93,23 @@ function Join-Cluster
 
 function Set-ConsulKV
 {
+    $ErrorActionPreference = 'Stop'
+
     Write-Output "Setting consul key-values ..."
 
     # Load config/services/consul
     & consul kv put -http-addr=http://127.0.0.1:8550 config/services/consul/datacenter 'test-integration'
     & consul kv put -http-addr=http://127.0.0.1:8550 config/services/consul/domain 'integrationtest'
 
-    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/consul/statsd/rules '\"*.*.* measurement.measurement.field\",'
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/consul/metrics/statsd/rules '\"consul.*.*.* .measurement.measurement.field\",'
 
     # Explicitly don't provide a metrics address because that means telegraf will just send the metrics to
     # a black hole
     & consul kv put -http-addr=http://127.0.0.1:8550 config/services/metrics/databases/system 'system'
     & consul kv put -http-addr=http://127.0.0.1:8550 config/services/metrics/databases/statsd 'services'
+
+    # load config/services/proxy.forwarder
+    & consul kv put -http-addr=http://127.0.0.1:8550 config/services/proxy.forwarder/metrics/statsd/rules '\"fabio.*.*.* .measurement.measurement.field\",'
 
     # load config/services/queue
     & consul kv put -http-addr=http://127.0.0.1:8550 config/services/queue/protocols/http/host 'http.queue'
@@ -88,17 +127,21 @@ function Set-ConsulKV
 
 function Set-VaultSecrets
 {
+    $ErrorActionPreference = 'Stop'
+
     Write-Output 'Setting vault secrets ...'
 
     # secret/services/queue/logs/syslog
-
-    # secret/services/jobs/encrypt
-
-    # secret/services/jobs/token
 }
 
 function Start-TestConsul
 {
+    [CmdletBinding()]
+    param(
+    )
+
+    $ErrorActionPreference = 'Stop'
+
     if (-not (Test-Path /test/consul))
     {
         New-Item -Path /test/consul -ItemType Directory | Out-Null
@@ -115,10 +158,17 @@ function Start-TestConsul
 
 function Start-TestVault
 {
+    [CmdletBinding()]
+    param(
+    )
+
+    $ErrorActionPreference = 'Stop'
+
     Write-Output "Starting vault ..."
-    Start-Process `
+    $process = Start-Process `
         -FilePath "/test/vault/vault" `
         -ArgumentList "-dev" `
-        -RedirectStandardOutput /test/vault/output.out `
-        -RedirectStandardError /test/vault/error.out
+        -PassThru `
+        -RedirectStandardOutput /test/vault/vaultoutput.out `
+        -RedirectStandardError /test/vault/vaulterror.out
 }
